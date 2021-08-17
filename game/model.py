33788fc80a5ed732577ab72a31ac97e9d73eb89a
jiang0906 <87628036+jiang0906@users.noreply.github.com>
2021-08-18 01:52:58 +0800

    Add files via upload

--- game/model.py
+++ game/model.py
@@ -1,21 +1,34 @@
+from __future__ import annotations
 import pygame
 import os
-from tower.towers import Tower, Vacancy
-from enemy.enemies import EnemyGroup
+import time
+from tower.tower_factory import Tower, Vacancy
+from enemy.enemy import EnemyGroup
 from menu.menus import UpgradeMenu, BuildMenu, MainMenu
-from game.user_request import RequestSubject, TowerFactory, TowerSeller, TowerDeveloper, EnemyGenerator, Muse, Music
-from settings import WIN_WIDTH, WIN_HEIGHT, BACKGROUND_IMAGE
+from game.user_request import RequestSubject, EnemyGenerator, TowerFactory, TowerSeller, TowerDeveloper, Muse, Music
+from game.stop import *
+from select_level import SelectLevel
+from settings import WIN_WIDTH, WIN_HEIGHT, BACKGROUND_IMAGE, STOP_IMAGE
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from menu.menus import Menu
 
 
 class GameModel:
     def __init__(self):
         # data
-        self.bg_image = pygame.transform.scale(BACKGROUND_IMAGE, (WIN_WIDTH, WIN_HEIGHT))
-        self.__towers = [Tower.Alcohol(250, 380), Tower.RapidTest(180, 300)]
+        self.bg_image = BACKGROUND_IMAGE
+        self.stop_button = Stop(1000, 575)
+        self.level = SelectLevel()
+        self.__towers = []
         self.__enemies = EnemyGroup()
         self.__menu = None
         self.__main_menu = MainMenu()
-        self.__plots = [Vacancy(50, 350), Vacancy(350, 280)]
+
+        # 一開始的遊戲增加點位
+        self.__plots = [Vacancy(110, 448), Vacancy(331, 444),Vacancy(179, 160),Vacancy(291, 157),
+                        Vacancy(417, 163),Vacancy(338, 296),Vacancy(233, 295),Vacancy(142, 292),
+                        Vacancy(496, 294),Vacancy(584, 167),Vacancy(498, 444),Vacancy(582, 297)]
         # selected item
         self.selected_plot = None
         self.selected_tower = None
@@ -28,12 +41,18 @@ class GameModel:
         self.generator = EnemyGenerator(self.subject)
         self.muse = Muse(self.subject)
         self.music = Music(self.subject)
-        #
         self.wave = 0
-        self.money = 500
+        self.money =700
+        self.support = 50
+        self.notsupport = 10
+        self.year = 2021
+        self.month = 8
+        self.date = 0
+        self.max_date = 400
         self.max_hp = 10
         self.hp = self.max_hp
-        self.sound = pygame.mixer.Sound(os.path.join("sound", "sound.flac"))
+        self.timer = time.time()
+        #self.sound = pygame.mixer.Sound(os.path.join("sound", "sound.flac"))
 
     def user_request(self, user_request: str):
         """ add tower, sell tower, upgrade tower"""
@@ -43,9 +62,9 @@ class GameModel:
         """get keyboard response or button response"""
         # initial
         self.selected_button = None
-        # key event
+        '''# key event
         if events["keyboard key"] is not None:
-            return "start new wave"
+            return "start new wave"'''
         # mouse event
         if events["mouse position"] is not None:
             x, y = events["mouse position"]
@@ -58,6 +77,16 @@ class GameModel:
     def select(self, mouse_x: int, mouse_y: int) -> None:
         """change the state of whether the items are selected"""
         # if the item is clicked, select the item
+        if self.stop_button.is_stopped():  # 如果現在正在暫停當中
+            if self.stop_button.clicked(mouse_x, mouse_y):  # 又按下stop
+                self.stop_button.is_stop = False  # 就代表遊戲繼續
+            else:
+                return  # 否則遊戲還是暫停
+        else:
+            if self.stop_button.clicked(mouse_x, mouse_y):  # 如果現在遊戲進行中
+                self.stop_button.is_stop = True  # 按下stop便代表遊戲暫停
+                return  # 不接受其餘clicked指令
+
         for tw in self.__towers:
             if tw.clicked(mouse_x, mouse_y):
                 self.selected_tower = tw
@@ -84,6 +113,7 @@ class GameModel:
             if btn.clicked(mouse_x, mouse_y):
                 self.selected_button = btn
 
+
     def call_menu(self):
         if self.selected_tower is not None:
             x, y = self.selected_tower.rect.center
@@ -101,6 +131,27 @@ class GameModel:
     def enemies_advance(self):
         self.__enemies.advance(self)
 
+    def condition_update(self):
+        if self.check_game_over() is False:
+            self.date += 1
+            if self.date % self.max_date == 0:
+                self.date = 0
+                self.month += 1
+                self.support += 1
+                if self.month == 13:
+                    self.month = 1
+                    self.year += 1
+
+        if self.support > 100:
+            self.support = 100
+        if self.support + self.notsupport > 100:
+            self.notsupport = 100 - self.support
+
+    def check_game_over(self):
+        if self.year == 2023 and self.month == 1:
+            return True
+        return False
+
     @property
     def enemies(self):
         return self.__enemies
@@ -114,20 +165,9 @@ class GameModel:
         return self.__menu
 
     @menu.setter
-    def menu(self, new_menu):
+    def menu(self, new_menu: Menu):
         self.__menu = new_menu
 
     @property
     def plots(self):
         return self.__plots
-
-
-
-
-
-
-
-
-
-
-
--- game/user_request.py
+++ game/user_request.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
 import pygame
-from tower.towers import Tower, Vacancy
+import time
+from tower.tower_factory import Tower, Vacancy
+from select_level import SelectLevel
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from game.model import GameModel
 
 """This module is import in model.py"""
 
@@ -10,34 +16,43 @@ Once the subject updates, if will notify all the observer who has register the s
 
 
 class RequestSubject:
-    def __init__(self, model):
+    def __init__(self, model: GameModel):
         self.__observers = []
         self.model = model
 
-    def register(self, observer):
+    def register(self, observer: list):
         self.__observers.append(observer)
 
-    def notify(self, user_request):
+    def notify(self, user_request: str):
         for o in self.__observers:
             o.update(user_request, self.model)
 
 
 class EnemyGenerator:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """add new enemy"""
-        if user_request == "start new wave":
-            model.enemies.add(10)
-            model.wave += 1
+        if "easy" in model.level.button:
+            # 敵人會在選擇簡單後3秒出現，場上空的時候才會生成下一波病毒
+            if time.time() - model.timer >= 3 and model.enemies.is_empty():
+                model.enemies.add(10)
+                model.wave += 1
+                model.timer = time.time()
+        if "hard" in model.level.button:
+            # 敵人會在選擇困難後3秒出現，場上的病毒會一直隨機從各路線出來
+            if time.time() - model.timer >= 3:
+                model.enemies.add(10)
+                model.wave += 1
+                model.timer = time.time
 
 
 class TowerSeller:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """sell tower"""
         if user_request == "sell":
             x, y = model.selected_tower.rect.center
@@ -48,35 +63,36 @@ class TowerSeller:
 
 
 class TowerDeveloper:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """(Bonus.1) upgrade tower"""
         if user_request == "upgrade" and model.selected_tower.level < 5:
-            if model.money > model.selected_tower.get_cost():
-                model.selected_tower.level += 1
-                model.money -= 20
             # if the money > upgrade cost of the selected tower , level+1
             # use model.selected_tower to access the selected tower data
             # use model.money to access to money data
-        pass
 
+            #如果錢達到升級的要求,則塔可升級
+            if model.money >= model.selected_tower.get_upgrade_cost():
+                model.money -= model.selected_tower.get_upgrade_cost()
+                model.selected_tower.level += 1
 
 
 class TowerFactory:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
-        self.tower_name = ["pcr", "rapid test", "alcohol"]
+        self.tower_name = ["mask", "injection", "alcohol", "foreheadgun"]
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """add new tower"""
         for name in self.tower_name:
             if user_request == name:
                 x, y = model.selected_plot.rect.center
-                tower_dict = {"pcr": Tower.PCR(x, y), "rapid test": Tower.RapidTest(x, y), "alcohol": Tower.Alcohol(x, y)}
+                tower_dict = {"mask": Tower.Mask(x, y), "injection": Tower.Injection(x, y),
+                              "alcohol": Tower.Alcohol(x, y), "foreheadgun": Tower.Foreheadgun(x, y)}
                 new_tower = tower_dict[user_request]
-                if model.money > new_tower.get_cost():
+                if model.money >= new_tower.get_cost():
                     model.money -= new_tower.get_cost()
                     model.towers.append(new_tower)
                     model.plots.remove(model.selected_plot)
@@ -84,10 +100,10 @@ class TowerFactory:
 
 
 class Music:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """music on"""
         if user_request == "music":
             pygame.mixer.music.unpause()
@@ -95,10 +111,10 @@ class Music:
 
 
 class Muse:
-    def __init__(self, subject):
+    def __init__(self, subject: RequestSubject):
         subject.register(self)
 
-    def update(self, user_request: str, model):
+    def update(self, user_request: str, model: GameModel):
         """music off"""
         if user_request == "mute":
             pygame.mixer.music.pause()
